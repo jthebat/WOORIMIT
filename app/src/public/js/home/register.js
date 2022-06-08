3b31ae1cff5e7ee8113a7643776fba6028de80fa
jthebat <taein007@gmail.com>
2022-06-09 02:59:38 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -10,6 +10,10 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click",register);
 
 function register(){
+    if(!id.value) return alert("아이디를 입력해주십시오.");
+    if(psword.value!==confirmPsword.value){
+        return alert("비밀번호가 일치하지 않습니다.");
+    }
     const req ={
         id: id.value,
         name: name.value,
@@ -30,13 +34,14 @@ fetch("/register",{
     .then((res)=>res.json())
     .then((res)=>{
         if(res.success){
-            location.href = "/";
+            location.href = "/login";
         }
         else{
             alert(res.msg);
         }
-        .catch((err)=>{
+    })
+    .catch((err)=>{
             console.error("회원가입 중 에러 발생");
-        })
-    });
+        }
+    );
 }
